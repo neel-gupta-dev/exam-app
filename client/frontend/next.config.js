e74cbd24e993a39909b6b7045b0202106b109632
Neel Gupta <guptaneelhome@gmail.com>
2026-03-27 02:19:15 +0530

    basic changes 2

---
+++ client/frontend/next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+    ],
+  },
+  // Restore your build overrides
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+module.exports = nextConfig;
