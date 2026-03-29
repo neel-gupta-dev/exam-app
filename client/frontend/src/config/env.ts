6c50cbfb25b23db50869658d1cd547e536bba98e
Neel Gupta <guptaneelhome@gmail.com>
2026-03-29 16:41:01 +0530

    made changes for vercel error

--- client/frontend/src/config/env.ts
+++ client/frontend/src/config/env.ts
@@ -1,5 +1,3 @@
 const isProd = process.env.NODE_ENV === 'production';
 
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 
-  (isProd ? 'https://exam-app-production-7f5d.up.railway.app/api' : 'http://localhost:5000/api');
-
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || '/api';
