dc960ce3562ada7307dacef0fbeeb5d4a1b0643c
Neel Gupta <guptaneelhome@gmail.com>
2026-04-08 14:44:33 +0530

    fixed pdf issues

--- client/frontend/src/proxy.ts
+++ client/frontend/src/proxy.ts
@@ -43,7 +43,8 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - notes/pdfs (static notes pdfs)
      */
-    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|notes/pdfs).*)",
   ],
 };
