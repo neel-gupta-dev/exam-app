910083925d4476ca26c2023464070f6fa78e59d8
Neel Gupta <guptaneelhome@gmail.com>
2026-04-13 11:45:42 +0530

    tried to fix clarity

--- client/frontend/src/app/layout.tsx
+++ client/frontend/src/app/layout.tsx
@@ -6,9 +6,11 @@ import { GoogleAnalytics } from "@next/third-parties/google";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Toaster } from "sonner";
+import Clarity from "@microsoft/clarity";
 import ConsoleEasterEgg from "@/components/ConsoleEasterEgg";
 import "./globals.css";
-
+const projectId = "wawvue9wgw";
+Clarity.init(projectId);
 const montserrat = Montserrat({
   subsets: ["latin"],
   weight: ["700", "800"],
@@ -85,10 +87,10 @@ export default function RootLayout({
             <Script id="clarity-script" strategy="lazyOnload">
               {`
                 (function(c,l,a,r,i,t,y){
-                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                })(window, document, "clarity", "script", "w4cyy5rkht");
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "wawvue9wgw");
               `}
             </Script>
           </>
