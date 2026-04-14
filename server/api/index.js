a3b187ccdcac18cb36ccbde46ba161e1a088e938
Neel Gupta <guptaneelhome@gmail.com>
2026-04-14 19:54:01 +0530

    added files for later vercel migratation of backend

--- server/api/index.js
+++ server/api/index.js
@@ -173,12 +173,14 @@ app.use(notFound);
 app.use(errorHandler);
 
 // --- Start Server ---
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+if (!process.env.VERCEL) {
+  const port = process.env.PORT || 5000;
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 
-  // Run Janitor every 30 minutes
-  setInterval(closeExpiredSessions, 30 * 60 * 1000);
-});
+    // Run Janitor every 30 minutes
+    setInterval(closeExpiredSessions, 30 * 60 * 1000);
+  });
+}
 
 export default app;
