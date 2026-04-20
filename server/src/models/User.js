e51e3b59c48203e94f5191ab32a437a5844a1140
Neel Gupta <guptaneelhome@gmail.com>
2026-04-20 11:43:32 +0530

    increases bcrypt cost

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -210,7 +210,7 @@ userSchema.pre('save', async function () {
     return;
   }
   
-  const salt = await bcrypt.genSalt(10);
+  const salt = await bcrypt.genSalt(12);
   this.password = await bcrypt.hash(this.password, salt);
 });
 
